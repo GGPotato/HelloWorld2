5616b7861c31b2909159da4d6c454843e1eda4dc
Zachary Ouellette <Zachary.p.ouellette@maine.edu>
2020-01-29 11:10:17 -0500

    uncommented the two messages to simulat the lab

--- hello.java
+++ hello.java
@@ -6,11 +6,11 @@ public static void main (String args[])
 
 {
 //added the two comments to the master branch but eeded to make a new brach and add them that way
-//System.out.println("Hello World");
+System.out.println("Hello World");
 
 System.out.println("Here ye : important announcement");
 
-//System.out.println("Goodbye!");
+System.out.println("Goodbye!");
 
 
 
