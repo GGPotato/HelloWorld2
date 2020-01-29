0101cbd1f2d86a780fb2efca0124c7d8276dc0fd
Zachary Ouellette <Zachary.p.ouellette@maine.edu>
2020-01-29 11:09:18 -0500

    added the two starting messages in comments to simulate the branch

--- hello.java
+++ hello.java
@@ -5,12 +5,14 @@ class Hello
 public static void main (String args[])
 
 {
-
-System.out.println("Hello World");
+//added the two comments to the master branch but eeded to make a new brach and add them that way
+//System.out.println("Hello World");
 
 System.out.println("Here ye : important announcement");
 
-System.out.println("Goodbye!");
+//System.out.println("Goodbye!");
+
+
 
 
 }
